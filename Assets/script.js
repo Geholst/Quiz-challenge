90b7e7da40f75d8cd3afec1939dafa0e100c882f
Geholst <geholst@msn.com>
2023-04-02 00:13:58 -0700

    Add starter code and pseudocode

---
+++ Assets/script.js
@@ -0,0 +1,83 @@
+//define all questions and answers 
+
+//define var for tracking 
+//      track time 
+//      track questions 
+
+//create var to refrence DOM elements
+//      timer
+//      questions
+//      answers
+//      start button      
+//      users initials
+//      save button 
+//      high scores
+
+// function 
+//   start the quiz 
+//      hide start button
+//      show quiz container
+//      start timer
+//      dispaly countdown  on screen 
+//      display first question 
+
+//function
+//      rendering a question
+//          clear/remove previous question            
+//          getting first question
+//          add question to question container
+//          add button for each answer
+//          add answers to answer contain
+
+//function
+//        handle the quiz completion
+//          stop timer
+//          hide quiz container
+//          show end screen
+//          show time remaining as score            
+
+
+//function  (event listener)
+//      handle answer clicks
+//      
+//      if
+//      answer is WRONG
+//      subtract time from timer 
+//      make sure time is displayed on page correctly
+//      flash wrong answer message (setTimeout)
+//
+//      update to next question
+//      display question on page
+//
+//      if 
+//      question is last question
+//      trigger quiz completion
+
+//function
+//      tracking time
+//          subtract time 
+//          update the page 
+//          
+//      if
+//      timer hits zero
+//          trigger quiz completion
+
+//function
+//      save high scores
+//          get value of user input (name/initials)
+//          validate input
+//          retrieve existing data from local storage 
+//          update the high score data
+//          save updated data back to local storage
+//          redirect to start screen after save
+
+//function
+//      listening for key press    
+//          check if key pressed was enter for saving scores 
+
+
+//event listeners
+//      click start 
+//      click answers
+//      click save scores
+//      keyups
